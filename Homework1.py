431540b33684dbb287e946c4fb3538cde52dd3aa
Darius Dinu <dariusdinu19@gmail.com>
2024-03-02 13:46:13 +0200

    Fix for ex #2.

--- Homework1.py
+++ Homework1.py
@@ -9,24 +9,33 @@ print(m - 1)
 print("\n")
 
 #ex2
+#calcul u
 print("Ex2")
 u = u**(-m)
 print(u)
+
+#adunare neasociativa
 x = 1.0
 y = u / 10
 z = u / 10
+m=1
+while(x + y) + z == x + (y + z):
+    m+= 1
+    y= 10**(-m)
+    z= y
+
+print(f"x = {x}, y = {y}, z = {z}")
 print((x + y) + z)
 print(x + (y + z))
 print((x + y) + z != x + (y + z))
 
-
+#inmultire neasociativa
 x = 2
 m = 1
 while (x * y) * z == x * (y * z):
   m += 1
   y = 10**(-m)
   z = y
-
 
 print(f"x = {x}, y = {y}, z = {z}")
 print((x * y) * z)
