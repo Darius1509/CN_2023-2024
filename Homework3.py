59b885b8fb47bc6c83114ff620f07ae5d7b7571b
Darius Dinu <dariusdinu19@gmail.com>
2024-03-23 13:04:41 +0200

    added corrections for HW#4.

--- Homework3.py
+++ Homework3.py
@@ -4,7 +4,7 @@ import scipy.linalg
 
 
 #1.Sa se calculeze vectorul b∈R^n:
-def calculate_b(A, s, epsilon=0.0001):
+def calculate_b(A, s, eps=0.0001):
     n = A.shape[0]
 
     if A.shape[0] != A.shape[1]:
@@ -22,8 +22,8 @@ def calculate_b(A, s, epsilon=0.0001):
     return b
 
 
-A = np.array([[0., 0, 4], [1, 2, 3], [0, 1, 2]])
-s = np.array([3, 2, 1])
+A = np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]])
+s = np.array([3, 2, 1, 9, 4])
 print(f"\033[93mMatrix A:\n\033[00m{A}")
 print(f"\033[93mVector s:\n\033[00m{s}")
 b = calculate_b(A, s)
@@ -31,14 +31,14 @@ print(f"\033[93mVector b:\n\033[00m{b}")
 
 
 #2.Sa se implementeze descompunerea QR a matricei A folosind algoritmul lui Householder
-def calculate_QR(A, epsilon=0.0001):
+def calculate_QR(A, eps=0.0001, print_P=False):
     A = A.astype(float)
     n = A.shape[0]
     Q = np.identity(n)
     u = np.zeros(n)
     for r in range(n - 1):
         alpha = sum([A[i][r] ** 2 for i in range(r, n)])
-        if alpha <= epsilon:
+        if alpha <= eps:
             break
         k = math.sqrt(alpha)  #norm of the vector
         if A[r][r] >= 0:
@@ -48,27 +48,33 @@ def calculate_QR(A, epsilon=0.0001):
         u[r] = A[r][r] - k
         for i in range(r + 1, n):
             u[i] = A[i][r]
-        for j in range(r + 1, n):
-            γ = sum([u[i] * A[i][j] for i in range(r, n)]) / beta
+        u_vec = u[:, np.newaxis]  # Make u a column vector
+        P = np.identity(n) - 2 * np.dot(u_vec, u_vec.T) / np.dot(u_vec.T, u_vec)
+        if print_P:
+            print(f"Reflection matrix P (or H) for iteration {r}:")
+            print(P)
+        for j in range(r, n):
+            gamma = sum([u[i] * A[i][j] for i in range(r, n)]) / beta
             for i in range(r, n):
-                A[i][j] = A[i][j] - γ * u[i]
+                A[i][j] = A[i][j] - gamma * u[i]
         # Transform column r of A
         A[r][r] = k
         for i in range(r + 1, n):
             A[i][r] = 0
-
+        # Update Q
         for j in range(n):
-            γ = sum([u[i] * Q[i][j] for i in range(r, n)]) / beta
+            gamma = sum([u[i] * Q[i][j] for i in range(r, n)]) / beta
             for i in range(r, n):
-                Q[i][j] = Q[i][j] - γ * u[i]
+                Q[i][j] = Q[i][j] - gamma * u[i]
     return Q.T, A
 
 
-Q, R = calculate_QR(np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]]))
+
+Q, R = calculate_QR(np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]]), print_P=True)
 print(f"\033[93mMatrix Q:\n\033[00m{Q}")
 print(f"\033[93mMatrix R:\n\033[00m{R}")
 
-Q, R = scipy.linalg.qr(np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]]))
+Q, R = scipy.linalg.qr(np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]]))
 print(f"\033[93mScipy Matrix Q:\n\033[00m{Q}")
 print(f"\033[93mScipy Matrix R:\n\033[00m{R}")
 
@@ -92,10 +98,10 @@ def inverse_substitution_method(A, b):
         x[i] /= A[i][i]
     return x
 
-def approximate_matrix_inverse_householder(A, epsilon=0.0001):
+def approximate_matrix_inverse_householder(A, eps=0.0001):
     A = A.astype(float)
     R = A.copy()
-    Q, R = calculate_QR(R, epsilon)
+    Q, R = calculate_QR(R, eps)
     n = R.shape[0]
     if np.linalg.det(R) == 0:
         print("The matrix is singular")
@@ -109,27 +115,27 @@ def approximate_matrix_inverse_householder(A, epsilon=0.0001):
             A[i][j] = x[i]
     return A
 
-def solve_system_householder_QR(A, b, epsilon=0.0001):
-    Q, R = calculate_QR(A, epsilon)
+def solve_system_householder_QR(A, b, eps=0.0001):
+    Q, R = calculate_QR(A, eps)
     x = inverse_substitution_method(R, Q.T @ b)
     return x
 
-A_init = np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]])
-s = np.array([3, 2, 1])
+A_init = np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]])
+s = np.array([3, 2, 1, 9, 4])
 b_init = calculate_b(A_init, s)
 
 x_Householder = solve_system_householder_QR(
-    np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]]), b_init)
+    np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]]), b_init)
 print(b_init)
 x_QR = solve_system_scipy_QR(
-    np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]]), b_init)
+    np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]]), b_init)
 
 print(f"\033[93mHouseholder QR solution:\n\033[00m", x_Householder)
 print(f"\033[93mScipy QR solution:\n\033[00m", x_QR)
 print(f"\033[93mNorm of the difference between the two solutions: \033[00m{np.linalg.norm(x_QR - x_Householder)}")
 
 #4.Sa se calculeze si sa se afiseze urmatoarele erori
-s = np.array([3, 2, 1])
+s = np.array([3, 2, 1, 9, 4])
 A = A_init.copy()
 b = b_init.copy()
 print("\033[93mnorm(A_init @ x_Householder - b_init) =\033[00m",
@@ -145,11 +151,11 @@ print("\033[93mnorm(x_QR - s)/ norm(s) =\033[00m",
 
 #5.Sa se calculeze inversa matricei A folosind descompunerea QR
 x = approximate_matrix_inverse_householder(
-    np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]]))
+    np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]]))
 
-print(f"\033[93mNumpy inverse of the matrix:\n\033[00m{np.linalg.inv(np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]]))}")
+print(f"\033[93mNumpy inverse of the matrix:\n\033[00m{np.linalg.inv(np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]]))}")
 print(f"\033[93mApproximate inverse of the matrix:\n\033[00m{x}")
-print(f"\033[93mNorm of the difference between the two inverses: \033[00m{np.linalg.norm(np.linalg.inv(np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]])) - x, ord=2)}")
+print(f"\033[93mNorm of the difference between the two inverses: \033[00m{np.linalg.norm(np.linalg.inv(np.array([[0, 0, 4, 8, 9], [1, 2, 3, 0, 2], [0, 1, 2, 4, 3], [1,6,7, 9, 1], [1,5,7,0,1]])) - x, ord=2)}")
 
 def generate_random_square_matrix_and_vector(n):
     A = np.random.randint(1, 10, (n, n))
@@ -169,25 +175,25 @@ def multiplication_RQ(R, Q):
     return result
 
 
-def equal_matrices(A, B, epsilon=1e-8):
+def equal_matrices(A, B, eps=1e-8):
     n = A.shape[0]
     for i in range(n):
         for j in range(n):
-            if abs(A[i][j] - B[i][j]) > epsilon:
+            if abs(A[i][j] - B[i][j]) > eps:
                 return False
     return True
 
 
-def approximate_limit(A_init, epsilon=1e-8, k_max=10000):
+def approximate_limit(A_init, eps=1e-8, k_max=10000):
     A = A_init.copy()
     k = 0
     while True:
         Q, R = calculate_QR(A)
         A_next = multiplication_RQ(R, Q)
-        # if equal_matrices(A, A_next, epsilon):
+        # if equal_matrices(A, A_next, eps):
         #     A = A_next
         #     break
-        if np.allclose(A, A_next, atol=epsilon):
+        if np.allclose(A, A_next, atol=eps):
             A = A_next
             break
         A = A_next
@@ -212,157 +218,3 @@ print(f"\nSame matrix for {k} iterations")
 print(matrix)
 
 
-# import time
-# import matplotlib.pyplot as plt
-#
-#
-# def solve_scipy_lu(A, b):
-#     lu, piv = scipy.linalg.lu_factor(A)
-#     return scipy.linalg.lu_solve((lu, piv), b)
-#
-#
-# def plot_solve_comparison(start, finish):
-#     solve_diff_householder_scipyqr = np.array([])
-#     solve_diff_householder_solve = np.array([])
-#     solve_diff_householder_scipylu = np.array([])
-#     solve_diff_householder_npsolve = np.array([])
-#
-#     time_householder = np.array([])
-#     time_scipyqr = np.array([])
-#     time_solve = np.array([])
-#     time_scipylu = np.array([])
-#     time_npsolve = np.array([])
-#
-#     fig, ax = plt.subplots(6, 1, figsize=(15, 10))
-#     for i in range(start, finish):
-#         A_init, s = generate_random_square_matrix_and_vector(i)
-#         b = calculate_b(A_init, s)
-#         A = A_init.copy()
-#
-#         t_start = time.time()
-#         x_Householder = solve_system_householder_QR(A, b)
-#         t_end = time.time()
-#         time_householder = np.append(time_householder, t_end - t_start)
-#
-#         t_start = time.time()
-#         solve_diff_householder_scipyqr = np.append(
-#             solve_diff_householder_scipyqr, np.linalg.norm(x_Householder - solve_system_scipy_QR(A_init, s)))
-#         t_end = time.time()
-#         time_scipyqr = np.append(time_scipyqr, t_end - t_start)
-#
-#         t_start = time.time()
-#         solve_diff_householder_solve = np.append(
-#             solve_diff_householder_solve, np.linalg.norm(
-#                 x_Householder - scipy.linalg.solve(A_init, s))
-#         )
-#         t_end = time.time()
-#         time_solve = np.append(time_solve, t_end - t_start)
-#
-#         t_start = time.time()
-#         solve_diff_householder_scipylu = np.append(
-#             solve_diff_householder_scipylu, np.linalg.norm(x_Householder - solve_scipy_lu(A_init, s)))
-#         t_end = time.time()
-#         time_scipylu = np.append(time_scipylu, t_end - t_start)
-#
-#         t_start = time.time()
-#         solve_diff_householder_npsolve = np.append(
-#             solve_diff_householder_npsolve, np.linalg.norm(
-#                 x_Householder - np.linalg.solve(A_init, s)))
-#         t_end = time.time()
-#         time_npsolve = np.append(time_npsolve, t_end - t_start)
-#
-#     ax[0].plot(np.arange(start, finish),
-#                solve_diff_householder_scipyqr, label="scipy QR")
-#     ax[0].plot(np.arange(start, finish),
-#                solve_diff_householder_solve, label="scipy solve")
-#     ax[0].plot(np.arange(start, finish),
-#                solve_diff_householder_scipylu, label="scipy LU")
-#     ax[0].plot(np.arange(start, finish),
-#                solve_diff_householder_npsolve, label="np solve")
-#     ax[0].set_title("Solve comparison")
-#     ax[0].set_xlabel("Dimension")
-#     ax[0].set_ylabel("Norm")
-#     ax[0].legend()
-#
-#     ax[1].plot(np.arange(start, finish),
-#                solve_diff_householder_scipyqr, label="scipy QR")
-#     ax[1].set_title("Solve comparison scipy QR")
-#     ax[1].set_xlabel("Dimension")
-#     ax[1].set_ylabel("Norm")
-#
-#     ax[2].plot(np.arange(start, finish),
-#                solve_diff_householder_solve, label="scipy solve")
-#     ax[2].set_title("Solve comparison scipy solve")
-#     ax[2].set_xlabel("Dimension")
-#     ax[2].set_ylabel("Norm")
-#
-#     ax[3].plot(np.arange(start, finish),
-#                solve_diff_householder_scipylu, label="scipy LU")
-#     ax[3].set_title("Solve comparison scipy LU")
-#     ax[3].set_xlabel("Dimension")
-#     ax[3].set_ylabel("Norm")
-#
-#     ax[4].plot(np.arange(start, finish),
-#                solve_diff_householder_npsolve, label="numpy solve")
-#     ax[4].set_title("Solve comparison numpy solve")
-#     ax[4].set_xlabel("Dimension")
-#     ax[4].set_ylabel("Norm")
-#
-#     ax[5].plot(np.arange(start, finish),
-#                time_householder, label="Householder")
-#     ax[5].plot(np.arange(start, finish),
-#                time_scipyqr, label="scipy QR")
-#     ax[5].plot(np.arange(start, finish),
-#                time_scipylu, label="scipy LU")
-#     ax[5].plot(np.arange(start, finish),
-#                time_npsolve, label="numpy solve")
-#     ax[5].set_title("Time comparison")
-#     ax[5].set_xlabel("Dimension")
-#     ax[5].set_ylabel("Time")
-#     ax[5].legend()
-#
-#     plt.show()
-#
-#
-# # All plots compare the householder implemented in the homework with the algorithm in the plot legend/title
-# plot_solve_comparison(1, 100)
-#
-#
-# def plot_inv_comparison(start, finish):
-#     fig, ax = plt.subplots(2, 1, figsize=(15, 10))
-#     inv_difference = np.array([])
-#     inv_difference_non_singular = np.array([])
-#     colors_non_singular = np.array([])
-#     for i in range(start, finish):
-#         A_init, s = generate_random_square_matrix_and_vector(i)
-#         inv_householder = approximate_matrix_inverse_householder(A_init)
-#         inv_difference = np.append(inv_difference, np.linalg.norm(
-#             np.linalg.pinv(A_init) - inv_householder))
-#         if np.linalg.det(A_init) != 0:
-#             inv_difference_non_singular = np.append(inv_difference_non_singular, np.linalg.norm(
-#                 np.linalg.inv(A_init) - inv_householder))
-#             colors_non_singular = np.append(colors_non_singular, 'r')
-#         else:
-#             inv_difference_non_singular = np.append(
-#                 inv_difference_non_singular, inv_difference[-1])
-#             colors_non_singular = np.append(colors_non_singular, 'b')
-#
-#     ax[0].plot(np.arange(start, finish), inv_difference)
-#     ax[0].set_title("Difference between scipy inverse and householder inverse")
-#     ax[0].set_xlabel("Matrix dimension")
-#     ax[0].set_ylabel("Norm of the difference")
-#
-#     if np.all(colors_non_singular == 'r'):
-#         ax[1].plot(np.arange(start, finish), inv_difference_non_singular)
-#     else:
-#         ax[1].scatter(np.arange(start, finish),
-#                       inv_difference_non_singular, c=colors_non_singular)
-#     ax[1].set_title(
-#         "Difference between scipy inverse and householder inverse for non singular matrices")
-#     ax[1].set_xlabel("Matrix dimension")
-#     ax[1].set_ylabel("Norm of the difference")
-#
-#     plt.show()
-#
-#
-# plot_inv_comparison(1, 100)
--- Homework4.py
+++ Homework4.py
@@ -12,10 +12,12 @@ def parse_vector(file_name):
             for index in range(n):
                 b[index] = float(file.readline())
 
+        print(f"Vectorul din {file_name} a fost incarcat cu succes.")
         return b
     except IOError:
         print("Could not open file: ", file_name)
         return None
+
 
 
 def parse_sparse_matrix(file_name):
@@ -25,7 +27,6 @@ def parse_sparse_matrix(file_name):
             first_line = True
             for text_line in file:
                 if first_line:
-                    # initialize matrix and dimension
                     n = int(text_line)
                     matrix = [dict() for i in range(n)]
                     first_line = False
@@ -43,21 +44,53 @@ def parse_sparse_matrix(file_name):
                 previous_value = matrix[line].setdefault(column, 0)
                 matrix[line][column] = previous_value + value
 
+        print(f"Matricea rara din {file_name} a fost incarcata cu succes folosind metoda de memorare dictionar in lista.")
         return matrix
     except IOError:
         print("Could not open file: ", file_name)
         return None
 
 
-def check_diagonal_elements(A, ε=0.00001):
-    # Check for each diagonal(main and secondary) that the elements are not 0
-    # Of course,we consider an element to be 0 if abs(value)<=ε
+def parse_sparse_matrix_rare(file_name):
+    sparse_matrix_rare = []
+    try:
+        with open(file_name, 'r') as file:
+            n = int(file.readline().strip())
+            for _ in range(n):
+                sparse_matrix_rare.append([])
+
+            for line in file:
+                parts = [x.strip() for x in line.strip().split(',')]
+                if len(parts) == 3:
+                    value, row, column = parts
+                    value = float(value)
+                    row, column = int(row), int(column)
+                    if value != 0:
+                        sparse_matrix_rare[row].append((value, column))
+                else:
+                    print(f"Linie neasteptata (ignorata): {line.strip()}")
+
+        print(f"Matricea rara din {file_name} a fost incarcata cu succes.")
+        return sparse_matrix_rare
+    except IOError:
+        print(f"Nu s-a putut deschide fișierul: {file_name}")
+        return None
+
+
+def print_sparse_matrix_rare(sparse_matrix_rare):
+    for row in sparse_matrix_rare:
+        print("[", end="")
+        for value, column in row:
+            print(f"({value}, {column})", end=" ")
+        print("]")
+
+
+def check_diagonal_elements(A, eps=0.00001):
     not_null_main_diagonal = not_null_secondary_diagonal = True
 
     n = len(A)
 
     for line in range(n):
-        # the elements for the both diagonals
         main_element = secondary_element = 0.0
         for column, value in A[line].items():
             if line == column:
@@ -65,10 +98,10 @@ def check_diagonal_elements(A, ε=0.00001):
             if line + column == n - 1:
                 secondary_element += value
 
-        if abs(main_element) <= ε:
+        if abs(main_element) <= eps:
             not_null_main_diagonal = False
 
-        if abs(secondary_element) <= ε:
+        if abs(secondary_element) <= eps:
             not_null_secondary_diagonal = False
 
         if (not not_null_main_diagonal) and (not not_null_secondary_diagonal):
@@ -77,14 +110,14 @@ def check_diagonal_elements(A, ε=0.00001):
     return not_null_main_diagonal, not_null_secondary_diagonal
 
 
-def approximate_solution_using_Gauss_Seidel(A, b, max_k=1000, max_difference=1e8, ε=1e-25):
+def approximate_solution_using_Gauss_Seidel(A, b, max_k=1000, max_difference=1e8, eps=1e-25):
     not_null_main_diagonal, not_null_secondary_diagonal = check_diagonal_elements(
-        A, ε)
+        A, eps)
 
     if not not_null_main_diagonal:
         print(
             "Diagonala principala trebuie sa contina elemente nenule pentru a putea aproxima folosind metoda Gauss-Seidel")
-        return False, "Divergenta"
+        return False, "Divergenta", 0
 
     n = b.shape[0]
     x = np.zeros(n).astype(float)
@@ -96,12 +129,8 @@ def approximate_solution_using_Gauss_Seidel(A, b, max_k=1000, max_difference=1e8
         for i in range(n):
             new_x_i = b[i]
 
-            # for j, value in A[i].items():
-            #     if i != j:
-            #         new_x_i -= value*x[j]
             new_x_i -= sum([value * x[j] for j, value in A[i].items() if i != j])
 
-            # if A[i][i] is 0,then we consider it to be 1
             new_x_i /= A[i].get(i, 1.0)
 
             euclidean_norm += (x[i] - new_x_i) ** 2
@@ -110,21 +139,19 @@ def approximate_solution_using_Gauss_Seidel(A, b, max_k=1000, max_difference=1e8
         euclidean_norm = math.sqrt(euclidean_norm)
         current_k += 1
 
-        if current_k == max_k or euclidean_norm < ε or euclidean_norm > max_difference:
+        if current_k >= max_k or euclidean_norm < eps or euclidean_norm > max_difference:
             break
 
-    if euclidean_norm < ε:
-        return True, x
+    if euclidean_norm < eps:
+        return True, x, current_k
 
-    return False, "Divergenta"
+    return False, "Divergenta", current_k
+
 
 
 def compute_solutions(file_name):
-    # Compute the solutions x for the files recognised
     x = None
-    if file_name == "a_pdf.txt" or file_name == "b_pdf.txt":
-        x = np.array([0.05658537, 0.06402825, 0.08, 0.08802333, 0.00808625])
-    elif file_name == "a_1.txt" or file_name == "b_1.txt":
+    if file_name == "a_1.txt" or file_name == "b_1.txt":
         n = 60690
         x = np.zeros((n,))
         for i in range(n):
@@ -167,7 +194,6 @@ def multiple_sparse_matrix_with_vector(matrix, vector):
 
     result = np.zeros(n)
     for line in range(n):
-        # Approximate the value of the element b[i]
         element = 0
         for column, value in matrix[line].items():
             element += value * vector[column]
@@ -176,21 +202,13 @@ def multiple_sparse_matrix_with_vector(matrix, vector):
     return result
 
 
-def compute_closeness(A, B, ε=1e-5):
-    # compute if 2 sparse matrix are equal element-wise
+def compute_closeness(A, B, eps=1e-5):
 
     if len(A) != len(B):
         print("Dimensiunea matricelor patratice trebuie sa fie aceeasi!")
         return
 
     n = len(A)
-
-    # Not a bad idea,but is dependent of the ε used when reading the matrix of A and B
-    # for line in range(n):
-    #   symmetric_differece_elements=A[line].keys()^B[line].keys()
-    #   if len(symmetric_differece_elements)>0:
-    #     # Found a column with a value different from the other one
-    #     return False
 
     for line in range(n):
         columns = A[line].keys() | B[line].keys()
@@ -198,66 +216,65 @@ def compute_closeness(A, B, ε=1e-5):
         for column in columns:
             a = A[line].get(column, 0)
             b = B[line].get(column, 0)
-            if abs(a - b) > ε:
+            if abs(a - b) > eps:
                 return False
 
     return True
 
 
-def run_test(a_file_name, b_file_name, max_k=10000, max_difference=1e8, ε=1e-5):
+def run_test(a_file_name, b_file_name, max_k=10000, max_difference=1e8, eps=1e-5):
     print("\n\nPentru fisierele " + a_file_name + " si " + b_file_name)
-    print("Folosim precizia ε=" + str(ε))
+    print("Folosim precizia eps=" + str(eps))
 
     b = parse_vector(b_file_name)
     A = parse_sparse_matrix(a_file_name)
+    A_alternative = parse_sparse_matrix_rare(a_file_name)
 
     if A is None or b is None:
         return
 
-    # print(A)
-    # print(b)
-
     if len(A) != len(b):
-        print("Dimensiunile pentru matricea A si b trebuie sa fie lafel!")
+        print("Dimensiunile pentru matricea A si b trebuie sa fie la fel!")
         return
 
     print("\nDimensiunea N = " + str(len(b)))
+    #print("\nMatricea A salvata economic(metoda enunt):" + print_sparse_matrix_rare(A_alternative))
 
-    not_null_main_diagonal, not_null_secondary_diagonal = check_diagonal_elements(
-        A, ε)
-    print("\nAre diagonala principala toate elementele nenule? " +
-          ("Da" if not_null_main_diagonal else "Nu"))
-    print("Are diagonala secundara toate elementele nenule? " +
-          ("Da" if not_null_secondary_diagonal else "Nu"))
+    not_null_main_diagonal, not_null_secondary_diagonal = check_diagonal_elements(A, eps)
+    print("\nAre diagonala principala toate elementele nenule? " + ("Da" if not_null_main_diagonal else "Nu"))
+    print("Are diagonala secundara toate elementele nenule? " + ("Da" if not_null_secondary_diagonal else "Nu"))
 
-    converged, x_approximation = approximate_solution_using_Gauss_Seidel(
-        A, b, max_k, max_difference, ε)
+    converged, x_approximation, iterations = approximate_solution_using_Gauss_Seidel(A, b, max_k, max_difference, eps)
     if converged:
-        b_approximation = multiple_sparse_matrix_with_vector(A, x_approximation)
-        print("Norma Chebyshev(sau distanta pe tabla de sah) dintre b si b obtinut prin x* este: " +
-              str(own_Chebyshev_norm(b_approximation - b)))
+        print(f"Metoda a convergat in {iterations} iteratii.")
+        Ax_minus_b = multiple_sparse_matrix_with_vector(A, x_approximation) - b
+        infinity_norm = np.linalg.norm(Ax_minus_b, np.inf)
+        print("Norma infinita a diferentei dintre Ax_GS si b este: " + str(infinity_norm))
 
         x = compute_solutions(a_file_name)
-        print("Norma euclideana dintre x si x* este : " + str(np.linalg.norm(x_approximation - x)))
-        # print("Solutia aproximativa este: " + str(x_approximation))
+        # if x is not None and x_approximation.shape == x.shape:
+        #     print("Norma euclideana dintre x si x* este : " + str(np.linalg.norm(x_approximation - x)))
+        # else:
+        #     print("Cannot compute Euclidean norm due to mismatch in array dimensions.")
+        print("Solutia aproximativa este: " + str(x_approximation))
     else:
-        print("Metoda nu a convers la o solutie")
+        print(f"Metoda nu a convergat dupa {iterations} iteratii.")
+
+
 
 
 def run_test_for_files(matrix_file_names: list[(str, str)], max_k: int = 10_000, max_difference: float = 1e8,
-                       ε: float = 1e-9):
+                       eps: float = 1e-9):
     for file_name_a, file_name_b in matrix_file_names:
-        run_test(file_name_a, file_name_b, max_k=max_k, max_difference=max_difference, ε=ε)
+        run_test(file_name_a, file_name_b, max_k=max_k, max_difference=max_difference, eps=eps)
 
 
-file_names = [
-    ("a_pdf.txt", "b_pdf.txt"),
-    ("a_1.txt", "b_1.txt"),
+file_names = [("a_1.txt", "b_1.txt"),
     ("a_2.txt", "b_2.txt"),
     ("a_3.txt", "b_3.txt"),
     ("a_4.txt", "b_4.txt"),
     ("a_5.txt", "b_5.txt")]
-run_test_for_files(file_names, max_k=10_000, max_difference=1e8, ε=1e-9)
+run_test_for_files(file_names, max_k=10_000, max_difference=1e8, eps=1e-9)
 
 # Bonus
 def add_sparse_matrixes(A, B):
@@ -265,56 +282,37 @@ def add_sparse_matrixes(A, B):
         print("Dimensiunea matricelor patratice trebuie sa fie aceeasi!")
         return
 
-
     n = len(A)
-
 
     C = []
     for line in range(n):
         summed_elements = dict()
 
-
         columns = A[line].keys() | B[line].keys()
-        # Get all the common columns
         for column in columns:
             summed_elements[column] = A[line].get(
                 column, 0) + B[line].get(column, 0)
 
-
         C.append(summed_elements)
-
 
     return C
 
 
-def run_add_test(A_file_name, B_file_name, C_file_name, ε=1e-5):
-    print("\n\nPrecizia folosita ε=" + str(ε))
+def run_add_test(A_file_name, B_file_name, C_file_name, eps=1e-5):
+    print("\n\nPrecizia folosita eps=" + str(eps))
     print("Din fisierele " + A_file_name + " si " + B_file_name +
           " extragem cele 2 matrici A si B si suma lor fiind C.Acest C il verificam cu matricea din fisierul " + C_file_name)
 
-
     A = parse_sparse_matrix(A_file_name)
     B = parse_sparse_matrix(B_file_name)
-
-
     if A is None or B is None:
         return
-
-
-    # print(B)
-
-
     our_C = add_sparse_matrixes(A, B)
-
-
     correct_C = parse_sparse_matrix(C_file_name)
-
-
-    are_equal = compute_closeness(our_C, correct_C, ε)
+    are_equal = compute_closeness(our_C, correct_C, eps)
     if are_equal:
         print("Matricea obtinuta este egala cu cea din fisierul dat")
     else:
         print("Matricea obtinuta nu este egala cu cea din fisierul dat")
 
-
-run_add_test("a.txt", "b.txt", "aplusb.txt", ε=1e-9)
+run_add_test("a.txt", "b.txt", "aplusb.txt", eps=1e-9)
